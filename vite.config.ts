5c234a0cff64406bd5b774b9a0ae666ed1a8524b
Carlos Eduardo <128966054+cadusantosofc@users.noreply.github.com>
2026-03-12 01:26:29 -0300

    fix: explicit allowed hosts for vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['app.gsacreditus.com.br']
       },
       preview: {
         port: 8080,
